09e3704eb8d748bb213bb1fb1279812f0b74260a
srujan75 <srujanneeruganti@gmail.com>
2025-04-10 18:48:52 +0530

    updated code to get error messages

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.plan_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_linux_function_app" "functionapp" {
+  name                       = var.function_app_name
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,15 @@
+output "resource_group_name" {
+  value = var.resource_group
+}
+
+output "storage_account_name" {
+  value = var.storage_name
+}
+
+output "app_service_plan_name" {
+  value = var.plan_name
+}
+
 output "function_app_name" {
-  value = azurerm_linux_function_app.functionapp.name
+  value = var.function_app_name
 }
